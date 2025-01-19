a96523cf880d134a3e921ed6dc8c94c7395a4c3e
pouria mohammadalipourahari <aharipuria1@gmail.com>
2025-01-19 17:48:58 +0100

    path modified

--- glimpse/src/compute_rsa.py
+++ glimpse/src/compute_rsa.py
@@ -1,7 +1,7 @@
 import sys
 import os.path
 # Add path to find rsasumm
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 from pathlib import Path
 import pandas as pd
